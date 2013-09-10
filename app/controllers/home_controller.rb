0319d6d6c012e3fcf39077f6d679af2093bd0221
Rafael Bugajski <bugajski.r@gmail.com>
2013-09-10 00:36:38 -0700

    grasping at straws

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -16,6 +16,8 @@ class HomeController < ApplicationController
     @p2 = Page.where(:code=>'index')
     @p3 = Page.where(:code=>'index',:app_segment_id=>AppSegment::HOME_STUFF)
     @p  = @p3.first
+    @pp1 = Page.where(:app_segment_id=>AppSegment::HOME_STUFF,:published=>true,:active=>true,:name=>@code)
+    @pp  = @pp1.first
     
   end
 
